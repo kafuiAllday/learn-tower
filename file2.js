dcaa2defd545144569c6a305e4fa8280c13cbd93
kafuiAllday <kay4rikki@gmail.com>
2022-03-30 07:26:15 -0700

    changes made to portiflio files

---
+++ file2.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('button')
+let div = document.querySelector('div')
+let h1 = document.querySelector('h1')
+
+button.addEventListener('click',function(){
+    div.classList.toggle('shw')
+})
