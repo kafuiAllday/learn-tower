7fb8d11e02d557eb967e9d21a2915e2e3586e0c8
kafuiAllday <kay4rikki@gmail.com>
2022-04-06 10:51:25 -0700

    change made to nav

--- file2.js
+++ file2.js
@@ -3,5 +3,6 @@ let div = document.querySelector('div')
 let h1 = document.querySelector('h1')
 
 button.addEventListener('click',function(){
+    h1.classList.toggle('shw')
     div.classList.toggle('shw')
 })
